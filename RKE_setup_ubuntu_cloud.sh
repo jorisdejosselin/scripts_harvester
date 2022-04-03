fb7eb26223446b9d169895883a28eeebd1118f65
jorisdejosselin <j.j.dejosselindejong@gmail.com>
2022-04-03 16:28:59 +0200

    Update RKE_setup_ubuntu_cloud.sh

--- RKE_setup_ubuntu_cloud.sh
+++ RKE_setup_ubuntu_cloud.sh
@@ -1,3 +1,6 @@
 curl -sfL https://get.rke2.io | sh -
 systemctl enable --now rke2-server
-echo "export PATH="/var/lib/rancher/rke2/bin:$PATH"" >> bash.bashrc
+echo "export PATH="/var/lib/rancher/rke2/bin:$PATH"" >> /etc/bash.bashrc
+mkdir -p /home/ubuntu/.kube/
+cp /etc/rancher/rke2/rke2.yaml /home/ubuntu/.kube/config
+chown ubuntu: /home/ubuntu/.kube/config
