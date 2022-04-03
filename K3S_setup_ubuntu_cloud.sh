0791c0ef0c3b771221093820611042e18414a497
jorisdejosselin <j.j.dejosselindejong@gmail.com>
2022-04-03 17:09:19 +0200

    Update K3S_setup_ubuntu_cloud.sh

--- K3S_setup_ubuntu_cloud.sh
+++ K3S_setup_ubuntu_cloud.sh
@@ -2,6 +2,15 @@ echo "alias k=kubectl" >> /etc/bash.bashrc
 chown ubuntu: /etc/rancher/k3s/k3s.yaml
 snap install helm --classic
 
+# Set env variables
+export KUBECONFIG="/etc/rancher/k3s/k3s.yaml" ; echo "export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"" >> /etc/bash.bashrc
+
+# Install cert-manager
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.1/cert-manager.crds.yaml
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.5.1
+
 # install rancher
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 kubectl create namespace cattle-system
