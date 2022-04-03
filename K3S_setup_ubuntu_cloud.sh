674e655214e036d12eaff00be3f42bd6d239cff9
jorisdejosselin <j.j.dejosselindejong@gmail.com>
2022-04-03 16:56:07 +0200

    Update K3S_setup_ubuntu_cloud.sh

--- K3S_setup_ubuntu_cloud.sh
+++ K3S_setup_ubuntu_cloud.sh
@@ -1,2 +1,10 @@
 curl -sfL https://get.k3s.io | sh -
 echo "alias k=kubectl" >> /etc/bash.bashrc
+chown ubuntu: /etc/rancher/k3s/k3s.yaml
+snap install helm --classic
+
+# install rancher
+helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+kubectl create namespace cattle-system
+helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname=rancher.my.org --set replicas=2
+
