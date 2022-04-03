572c36a260c2bce51351f319e57bd417d67c7347
jorisdejosselin <j.j.dejosselindejong@gmail.com>
2022-04-03 16:36:41 +0200

    Update and rename RKE_setup_ubuntu_cloud.sh to K3S_setup_ubuntu_cloud.sh

---
+++ K3S_setup_ubuntu_cloud.sh
@@ -0,0 +1,2 @@
+curl -sfL https://get.k3s.io | sh -
+echo "alias k=kubectl" >> /etc/bash.bashrc
--- RKE_setup_ubuntu_cloud.sh
+++
@@ -1,6 +0,0 @@
-curl -sfL https://get.rke2.io | sh -
-systemctl enable --now rke2-server
-echo "export PATH="/var/lib/rancher/rke2/bin:$PATH"" >> /etc/bash.bashrc
-mkdir -p /home/ubuntu/.kube/
-cp /etc/rancher/rke2/rke2.yaml /home/ubuntu/.kube/config
-chown ubuntu: /home/ubuntu/.kube/config
