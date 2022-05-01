98bce3e0e6d321ecc4b7c0b44046c3bf52445567
jorisdejosselin <j.j.dejosselindejong@gmail.com>
2022-05-01 21:56:39 +0200

    Update K3S_setup_ubuntu_cloud.sh

--- K3S_setup_ubuntu_cloud.sh
+++ K3S_setup_ubuntu_cloud.sh
@@ -27,5 +27,5 @@ helm install cert-manager jetstack/cert-manager --namespace cert-manager --creat
 helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 kubectl create namespace cattle-system
 
-helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname=rancher.joict.nl --set replicas=2 --set ingress.tls.source=letsEncrypt --set letsEncrypt.email=joor-josselin@hotmail.com --set letsEncrypt.ingress.class=traefik
+helm install rancher rancher-latest/rancher --namespace cattle-system --set hostname=rancher.sub.joict.nl --set replicas=2 --set ingress.tls.source=letsEncrypt --set letsEncrypt.email=joor-josselin@hotmail.com --set letsEncrypt.ingress.class=traefik
 
